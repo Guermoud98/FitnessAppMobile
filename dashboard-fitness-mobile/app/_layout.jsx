cd39ebca17c08fcb7e0570e8d632fb3f21dd6d5d
@Neha-ETTALEBY <ettaleby.neha@gmail.com>
2024-05-19 23:08:54 +0100

    added exerciseDB in folder api

--- dashboard-fitness-mobile/app/_layout.jsx
+++ dashboard-fitness-mobile/app/_layout.jsx
@@ -5,6 +5,19 @@ import { Stack } from 'expo-router';
 export default function _layout() {
     return (
         <Stack screenOptions={{ headerShown: false }}>
-        </Stack>
-    );
+       
+    
+        <Stack.Screen name="exercises" options ={{
+            presentation:'fullScreenModal'
+
+        }} />
+
+        <Stack.Screen name="exerciseDetails" options ={{
+            presentation:'modal'
+
+        }} />
+
+
+     </Stack>
+)
 }
---
+++ dashboard-fitness-mobile/app/exerciseDetails.jsx
@@ -0,0 +1,10 @@
+import {View ,Text, TouchableOpacity} from 'react-native'
+import React, { version } from 'react'
+export default function exerciseDetails()
+{
+    const item =useLocalSearchParams();
+    console.log('got item ',item )
+    return(
+     <View></View>
+    )
+}
